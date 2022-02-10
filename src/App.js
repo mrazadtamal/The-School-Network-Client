1d248bbbce87cd2f6bb8eb9998d8effc3446728d
Abdul Al Joboyer Chy <55484176+Aljoboyer@users.noreply.github.com>
2022-02-10 18:31:43 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PrincipalDashboard from "./Pages/PrincipalPanel/PrincipalDashboard/PrincipalDashboard";
+import PrincipalHome from "./Pages/PrincipalPanel/PrincipalHome/PrincipalHome";
+import PrincipalPublishNotice from "./Pages/PrincipalPanel/PrincipalPublishNotice/.PrincipalPublishNotice";
+import TeachersPanel from "./Pages/TeachersPanel/TeachersPanel/TeachersPanel";
+
+import HomePage from "./Pages/AllUser/HomePageComponents/HomePage";
+import LogInPage from "./Pages/AllUser/Login/LogInPage";
 
 function App() {
   return (
     <div className="App">
-      <h1>Coding wizards</h1>
+      <BrowserRouter>
+        <Routes>
+          {/* --------------All User Homepage Routes Start--------------- */}
+
+          <Route path="/" element={<HomePage />}/>
+          <Route path="/login" element={<LogInPage/>}/>
+
+          {/* --------------All User Homepage Routes End--------------- */}
+          {/* --------------Principal Routes Start--------------- */}
+
+          <Route path="/PrincipalDashboard" element={<PrincipalDashboard />}>
+            <Route path="/PrincipalDashboard" element={<PrincipalHome />} />
+            <Route
+              path="/PrincipalDashboard/PrincipalPublishNotice"
+              element={<PrincipalPublishNotice />}
+            />
+          </Route>
+
+          {/* --------------Principal Routes End--------------- */}
+
+          {/* --------------Teacher Routes Start */}
+          <Route path="/TeachersDashboard" element={<TeachersPanel />}>
+            <Route
+              path="/TeachersDashboard"
+              element={<TeachersPanel />}
+            ></Route>
+          </Route>
+          {/* --------------Teacher Routes End-------------- */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
-
 export default App;
