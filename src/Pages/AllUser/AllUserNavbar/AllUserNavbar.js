2d9a3ecdf529ef3ae838d4f14b904f3d0b4fc020
Abdul Al Joboyer Chy <55484176+Aljoboyer@users.noreply.github.com>
2022-02-11 15:07:21 +0600

    Update and rename AllUserNavbar.jsx to AllUserNavbar.js

--- src/Pages/AllUser/AllUserNavbar/AllUserNavbar.js
+++ src/Pages/AllUser/AllUserNavbar/AllUserNavbar.js
@@ -3,7 +3,7 @@ import { GiHamburgerMenu } from "react-icons/gi";
 import { AiOutlineClose } from "react-icons/ai";
 
 import "./Navbar.css";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const AllUserNavbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
@@ -20,11 +20,9 @@ const AllUserNavbar = () => {
       </div>
       <ul className="app__navbar-links">
         <li className="p__opensans">
-          <a >
-              <Link to="/">
-                  Home
-              </Link>
-              </a>
+          <a href="#home">
+            <Link to="/">Home</Link>
+          </a>
         </li>
         <li className="p__opensans">
           <a href="#about">About</a>
@@ -38,24 +36,11 @@ const AllUserNavbar = () => {
         <li className="p__opensans">
           <a href="#contact">Contact</a>
         </li>
-          <li className="p__opensans">
-              <a>
-                  <Link to="/admissionPolicy">
-                     Admission
-                  </Link>
-              </a>
-          </li>
-         
-
       </ul>
       <div className="app__navbar-login">
-
-           <a  className="p__opensans">
-             <Link to="/login">
-                 SignIn
-             </Link>
-           </a>
-
+        <Link className="p__opensans" to="/login">
+          SignIn
+        </Link>
       </div>
 
       <div className="app__navbar-smallScreen">
