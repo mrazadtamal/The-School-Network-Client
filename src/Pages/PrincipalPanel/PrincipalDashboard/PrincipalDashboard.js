3f933ac4b6994d883d662636f0d31eb1c29c89b8
Abdul Al Joboyer Chy <55484176+Aljoboyer@users.noreply.github.com>
2022-02-13 21:17:23 +0600

    Update PrincipalDashboard.js

--- src/Pages/PrincipalPanel/PrincipalDashboard/PrincipalDashboard.js
+++ src/Pages/PrincipalPanel/PrincipalDashboard/PrincipalDashboard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import PrincipalNavbar from "../PrincipalNavbar/PrincipalNavbar";
 import PrincipalSidebar from "../PrincipalSidebar/PrincipalSidebar";
 
 const PrincipalDashboard = () => {
