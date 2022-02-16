5198173af523fa7c2f06816b3e07a7bcb65e0a2f
Ariful01303 <arifhossen01303@gmail.com>
2022-02-16 11:04:45 +0600

    complite

--- src/Pages/TeachersPanel/TeachersAssignment/TeachersAssignment.js
+++ src/Pages/TeachersPanel/TeachersAssignment/TeachersAssignment.js
@@ -1,37 +1,26 @@
 import React from 'react';
-import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import'./Teacherpanel.css'
 const TeachersAssignment = () => {
-    const [titles,setTitles]=useState('')
-    const [descript,setdescript]=useState('')
-    const [files,setFiles]=useState('')
-    const titleHandle=(e)=>{
-        setTitles(e.target.value)
-    } 
-    const descriptHandle=(e)=>{
-        setdescript(e.target.value)
-    }
-  const fileHandle=(e)=>{
-     setFiles(e.target.value)
-  }
     
-   const handleSubmit=()=>{
-       console.log(titles,descript,files)
-   }
+    const { register, handleSubmit,reset } = useForm();
+    const onSubmit = data =>{
+        console.log(data)
+        reset()
+    }
     return (
-        <div className='text-center w-full m-auto mt-5'>
+        <div>
+               <div className='text-center w-full m-auto mt-5'>
             
-          <div className='w-2/4 mx-auto container teacher-side p-5 '>
-          
-
-          <p class='text-3xl text-sky-400/100'>Published Assignment</p>
-                
-                <div>       
-                 
-                       <label class="block mx-2">
-                           <span class="sr-only">Choose File</span>
+               <div className='w-2/4 mx-auto container teacher-side p-5 '>
+            
+  
+                   <p class='text-3xl text-sky-400/100'>Published Assignment</p>
+                  
+            <form onSubmit={handleSubmit(onSubmit)}>
+                        <label class="block mx-2">
+                        <span class="sr-only">Choose File</span>
                         <input 
-                        onChange={fileHandle}
                         type="file" 
                         class="block w-full text-sm text-slate-500
                              file:mr-4 file:py-2 file:px-4
@@ -41,40 +30,37 @@ const TeachersAssignment = () => {
                              hover:file:bg-violet-100
                              "/>
                       </label>
+             <p className='mb-4'>Or</p>
 
-                       </div>
-                       <p className='mb-4'>Or</p>
-                  <label class="relative block mt-5 mb-2">
-                    
-                        <input 
-                          className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-                          onChange={titleHandle} 
-                          placeholder="Assignment" 
-                           type="text"
-                         name="title"/>
-                 </label>
+                      <label class="relative block mt-5 mb-2">
 
-                        <textarea
-                            name="assingmentDescription"
-                            onChange={descriptHandle}
-                            id=""
-                            rows="5"
-                            className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" 
-                            placeholder="Assignment Description" 
-                            type="text"
-                        ></textarea>
-                         
-                         <button 
-                          className="block  my-3 px-5   py-1 bg-cyan-500 hover:bg-cyan-600 rounded-lg text-gray-900 font-medium"
+                   <input 
+                   className="placeholder:italic
+                    placeholder:text-slate-400 block bg-white
+                     w-full border border-slate-300 rounded-md
+                      py-2 pl-9 pr-3 shadow-sm focus:outline-none
+                    focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+                    {...register("title")} placeholder="Title"/>
+                    </label> <br />
+                <textarea
+                rows="5"
+                className="placeholder:italic
+                placeholder:text-slate-400 block bg-white
+                 w-full border border-slate-300 rounded-md
+                  py-2 pl-9 pr-3 shadow-sm focus:outline-none
+                 focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+                type="text"
+                 {...register("description")} placeholder="Assignment Description"/> <br />
 
-                              onClick={handleSubmit}  
-                 
-                            >Publish
-                          </button> 
-                     
-                       
-                
-         </div>
+
+                    <input 
+                    className="block  my-3 px-5 
+                      py-1 bg-cyan-500 hover:bg-cyan-600
+                       rounded-lg text-gray-900 font-medium"
+                     type="submit" />
+        </form>
+          </div>
+        </div>
         </div>
     );
 };
