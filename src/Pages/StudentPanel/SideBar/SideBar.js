f97500a5e3e78f32ef492da2fb38bd528c2e635b
mrazadtamal <mrazadt@gmail.com>
2022-03-25 19:23:31 +0600

    3/25

--- src/Pages/StudentPanel/SideBar/SideBar.js
+++ src/Pages/StudentPanel/SideBar/SideBar.js
@@ -86,22 +86,6 @@ const SideBar = ({ children }) => {
   const navigate = useNavigate();
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
-  const inputAnimation = {
-    hidden: {
-      width: 0,
-      padding: 0,
-      transition: {
-        duration: 0.2,
-      },
-    },
-    show: {
-      width: "140px",
-      padding: "5px 15px",
-      transition: {
-        duration: 0.2,
-      },
-    },
-  };
 
   const showAnimation = {
     hidden: {
